885b0fb3bdcf1349eaf79ee08c9b1be794c93e9d
dilshansanjeewa <dilshansanjeewads7@gmail.com>
2026-01-05 10:25:23 +0530

    Add Bootstrap Icons and update Navbar component with new styling and logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,37 +1,90 @@
-import Logo from '../assets/logo/Apex_Assets_Logo.svg';
-// import UserImg from '';
-import "./css/navbar.css"
-
+import React, { useState } from "react";
+import logo from '../assets/logo/nav-logo.png'
+import './css/Navbar.css'
 function Navbar() {
-    return (
-        <>
-            <nav className="navbar navbar-expand-lg custom-navbar">
-                <div className="container-custom">
-                    {/* Logo Section */}
-                    <div className="navbar-brand-wrapper">
-                        <img src={Logo} alt="Apex Assets" className="logo-img" />
-                    </div>
 
-                    {/* Hamburger Menu for Mobile */}
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const [userName, setUserName] = useState("Dilshan Sanjeewa");
+
+    return (
+
+        <>
+            <nav className="navbar navbar-expand-lg navbar-light bg-white shadow-sm sticky-top">
+                <div className="container">
+
+                    <a className="navbar-brand" href="/">
+                        <img src={logo} alt="Apex Assets" className="nav-logo" />
+                    </a>
+
+                    <button
+                        className="navbar-toggler"
+                        type="button"
+                        data-bs-toggle="collapse"
+                        data-bs-target="#navbarContent"
+                        aria-controls="navbarContent"
+                        aria-expanded="false"
+                        aria-label="Toggle navigation"
+                    >
                         <span className="navbar-toggler-icon"></span>
                     </button>
 
-                    {/* Links and Actions */}
-                    <div className="collapse navbar-collapse" id="navbarNav">
-                        <ul className="navbar-nav mx-auto">
-                            <li className="nav-item"> <a className="nav-link custom-underline" href="#home">Home</a> </li>
-                            <li className="nav-item"> <a className="nav-link custom-underline" href="#about">About Us</a> </li>
-                            <li className="nav-item"> <a className="nav-link custom-underline" href="#contact">Contact</a> </li>
-                        </ul>
+                    <div className="collapse navbar-collapse" id="navbarContent">
+                        <ul className="navbar-nav ms-auto align-items-lg-center">
 
-                        {/* User Profile & Logout Group */}
-                        <div className="user-action-container d-flex align-items-center">
-                            <div className="user-avatar">
-                                {/* <img src={UserImg} alt="User" /> */}
-                            </div>
-                            <button className="btn-logout-aws">Log out</button>
-                        </div>
+                            {/* Menu Dropdown */}
+                            <li className="nav-item dropdown me-lg-4">
+                                <a
+                                    className="nav-link dropdown-toggle menu-link d-flex align-items-center"
+                                    href="#"
+                                    id="menuDropdown"
+                                    role="button"
+                                    data-bs-toggle="dropdown"
+                                    aria-expanded="false"
+                                >
+                                    {/* Bootstrap Menu Icon */}
+                                    <i className="bi bi-list me-2" style={{ fontSize: '1.2rem' }}></i>
+                                    Menu
+                                </a>
+                                <ul className="dropdown-menu dropdown-menu-end custom-dropdown" aria-labelledby="menuDropdown">
+                                    <li><a className="dropdown-item" href="/">Home</a></li>
+                                    <li><a className="dropdown-item" href="/listings">My Listings</a></li>
+                                    <li><a className="dropdown-item" href="/inquiries">Inquiries</a></li>
+                                    <li><a className="dropdown-item" href="/bookings">Bookings</a></li>
+                                    <li><a className="dropdown-item" href="/chat">Chat</a></li>
+                                    <li><a className="dropdown-item" href="/contact">Contact Us</a></li>
+                                    <li><a className="dropdown-item" href="/about">About Us</a></li>
+                                    <li><hr className="dropdown-divider" /></li>
+                                    <li className="px-3 py-2">
+                                        <button className="btn btn-post-ad w-100 text-light">POST NEW AD</button>
+                                    </li>
+                                </ul>
+                            </li>
+
+                            {/* Profile Section */}
+                            {isLoggedIn ? (
+                                <li className="nav-item dropdown">
+                                    <a className="nav-link d-flex align-items-center profile-trigger" href="#" id="profileDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                        <span className="me-2 user-name-text d-none d-lg-inline">{userName}</span>
+                                        <div className="profile-img-wrapper">
+                                            <img src="https://github.com/mdo.png" alt="Profile" className="rounded-circle profile-img" />
+                                        </div>
+                                    </a>
+                                    <ul className="dropdown-menu dropdown-menu-end custom-dropdown" aria-labelledby="profileDropdown">
+                                        <li><a className="dropdown-item" href="/profile">My Profile</a></li>
+                                        <li><a className="dropdown-item" href="/settings">Settings</a></li>
+                                        <li><hr className="dropdown-divider" /></li>
+                                        <li><a className="dropdown-item text-danger" href="/logout">Log out</a></li>
+                                    </ul>
+                                </li>
+                            ) : (
+                                <li className="nav-item">
+                                    <button className="btn-sign-in" onClick={() => setIsLoggedIn(true)}>
+                                        Sign In
+                                    </button>
+                                </li>
+                            )}
+
+                        </ul>
                     </div>
                 </div>
             </nav>
